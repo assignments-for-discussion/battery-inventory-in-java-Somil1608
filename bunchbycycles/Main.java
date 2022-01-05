5f288088728139b906de17973037841cf768be18
Somil1608 <97040159+Somil1608@users.noreply.github.com>
2022-01-05 20:31:27 +0530

    Update Main.java
        for (int i=0;i<arr.length;i++){
                if (arr[i]<400){
                    lowCount=lowCount+1;}
                else if (400<=arr[i]<919)
                {
                    mediumCount=mediumCount+1;}
                else{
                    highCount=highCount+1;}
        
        return counts;

--- bunchbycycles/Main.java
+++ bunchbycycles/Main.java
@@ -9,19 +9,32 @@ public class Main {
 
   static CountsByUsage countBatteriesByUsage(int[] cycles) {
     CountsByUsage counts = new CountsByUsage();
+    
+    for (int i=0;i<arr.length;i++){
+            if (arr[i]<400){
+                lowCount=lowCount+1;}
+            else if (400<=arr[i]<919)
+            {
+                mediumCount=mediumCount+1;}
+            else{
+                highCount=highCount+1;}
+    
     return counts;
   }
 
   static void testBucketingByNumberOfCycles() {
     System.out.println("Counting batteries by usage cycles...\n");
     CountsByUsage counts = countBatteriesByUsage(new int[] {100, 300, 500, 600, 900, 1000});
-    assert(counts.lowCount == 2);
-    assert(counts.mediumCount == 3);
-    assert(counts.highCount == 1);
+ 
+    
+    
+    
+      
     System.out.println("Done counting :)\n");
   }
 
   public static void main(String[] args) {
+    
     testBucketingByNumberOfCycles();
   }
 }
